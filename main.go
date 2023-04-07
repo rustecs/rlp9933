ab39962507e732f06f281fbafb53873f4f2ca9e1
Ruslan Kukoyashny <ruslan.tecs@gmail.com>
2023-04-07 14:33:33 +0300

    simple

--- main.go
+++ main.go
@@ -1,6 +1,31 @@
-packacge main
+package main
 
 import "fmt"
 
+const (
+	maxResult     int = 100
+	limitPerQuery int = 10
+)
+
+type config struct {
+}
+
 func main() {
+	fmt.Println("Start ...")
+
+	conf, err := initialConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	//for {
+	// get data
+	//}
+	fmt.Println("Done")
+}
+
+func initialConfig() (config, error) {
+	// initial pg
+	// read last status
+	return config{}, nil
 }
